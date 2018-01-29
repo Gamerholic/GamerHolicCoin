422f448e3eda6b5d112a1e6d755ac333609dfc50
Akshay <akshaycm@hotmail.com>
2018-01-29 18:02:25 +0300

    Revert "Changed name of coin"
    This reverts commit 25229c6ccf66166617dbc84e7a8f53816466d367.

--- src/net.cpp
+++ src/net.cpp
@@ -373,7 +373,7 @@ bool GetMyExternalIP(CNetAddr& ipRet)
 
             pszGet = "GET / HTTP/1.1\r\n"
                      "Host: checkip.dyndns.org\r\n"
-                     "User-Agent: OMOCoin\r\n"
+                     "User-Agent: GamerholicCoin\r\n"
                      "Connection: close\r\n"
                      "\r\n";
 
@@ -392,7 +392,7 @@ bool GetMyExternalIP(CNetAddr& ipRet)
 
             pszGet = "GET /simple/ HTTP/1.1\r\n"
                      "Host: www.showmyip.com\r\n"
-                     "User-Agent: OMOCoin\r\n"
+                     "User-Agent: GamerholicCoin\r\n"
                      "Connection: close\r\n"
                      "\r\n";
 
@@ -409,7 +409,7 @@ bool GetMyExternalIP(CNetAddr& ipRet)
 void ThreadGetMyExternalIP(void* parg)
 {
     // Make this thread recognisable as the external IP detection thread
-    RenameThread("OMOCoin-ext-ip");
+    RenameThread("gamerholiccoin-ext-ip");
 
     CNetAddr addrLocalHost;
     if (GetMyExternalIP(addrLocalHost))
@@ -634,7 +634,7 @@ void CNode::copyStats(CNodeStats &stats)
 void ThreadSocketHandler(void* parg)
 {
     // Make this thread recognisable as the networking thread
-    RenameThread("OMOCoin-net");
+    RenameThread("gamerholiccoin-net");
 
     try
     {
@@ -988,7 +988,7 @@ void ThreadSocketHandler2(void* parg)
 void ThreadMapPort(void* parg)
 {
     // Make this thread recognisable as the UPnP thread
-    RenameThread("OMOCoin-UPnP");
+    RenameThread("gamerholiccoin-UPnP");
 
     try
     {
@@ -1049,7 +1049,7 @@ void ThreadMapPort2(void* parg)
             }
         }
 
-        string strDesc = "OMOCoin " + FormatFullVersion();
+        string strDesc = "GamerholicCoin " + FormatFullVersion();
 #ifndef UPNPDISCOVER_SUCCESS
         /* miniupnpc 1.5 */
         r = UPNP_AddPortMapping(urls.controlURL, data.first.servicetype,
@@ -1148,7 +1148,7 @@ static const char *strDNSSeed[][2] = {
 void ThreadDNSAddressSeed(void* parg)
 {
     // Make this thread recognisable as the DNS seeding thread
-    RenameThread("OMOCoin-dnsseed");
+    RenameThread("gamerholiccoin-dnsseed");
 
     try
     {
@@ -1242,7 +1242,7 @@ void ThreadDumpAddress2(void* parg)
 void ThreadDumpAddress(void* parg)
 {
     // Make this thread recognisable as the address dumping thread
-    RenameThread("OMOCoin-adrdump");
+    RenameThread("gamerholiccoin-adrdump");
 
     try
     {
@@ -1257,7 +1257,7 @@ void ThreadDumpAddress(void* parg)
 void ThreadOpenConnections(void* parg)
 {
     // Make this thread recognisable as the connection opening thread
-    RenameThread("OMOCoin-opencon");
+    RenameThread("gamerholiccoin-opencon");
 
     try
     {
@@ -1438,7 +1438,7 @@ void ThreadOpenConnections2(void* parg)
 void ThreadOpenAddedConnections(void* parg)
 {
     // Make this thread recognisable as the connection opening thread
-    RenameThread("OMOCoin-opencon");
+    RenameThread("gamerholiccoin-opencon");
 
     try
     {
@@ -1569,7 +1569,7 @@ bool OpenNetworkConnection(const CAddress& addrConnect, CSemaphoreGrant *grantOu
 void ThreadMessageHandler(void* parg)
 {
     // Make this thread recognisable as the message handling thread
-    RenameThread("OMOCoin-msghand");
+    RenameThread("gamerholiccoin-msghand");
 
     try
     {
@@ -1737,7 +1737,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. OMOCoin is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. GamerholicCoin is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
@@ -1820,7 +1820,7 @@ void static Discover()
 void StartNode(void* parg)
 {
     // Make this thread recognisable as the startup thread
-    RenameThread("OMOCoin-start");
+    RenameThread("gamerholiccoin-start");
 
     if (semOutbound == NULL) {
         // initialize semaphore
